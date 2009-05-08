84f3d0558e8b34907cb291372b659f1dbadcd1fa
unknown <matt@mcurry.net>
2009-05-08 01:57:22 -0400

    removed cache dir

--- tests/test_app/webroot/cjs/test1_test2_test3_1241762014.js
+++
@@ -1,9 +0,0 @@
-/* test1.js (91%) */
-var str="I'm a string";alert(str);
-
-/* test2.js (69%) */
-var sum=0;for(i=0;i<100;i++){sum+=i;}
-alert(i);
-
-/* test3.js (89%) */
-$(function(){$("#nav").show();});
